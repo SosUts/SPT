7fab0e56c05838cc6812a78ec773e6f8b86cdef7
Sosuke <uts.199.ubosou@gmail.com>
2020-05-01 12:54:37 +0900

    Add rand for multiple sequences(fixed length)

--- src/baum_welch.jl
+++ src/baum_welch.jl
@@ -36,3 +36,36 @@ function likelihood!(dR, D, L, dt, error)
         end
     end
 end
+
+function rand(
+    rng::AbstractRNG, hmm::AbstractHMM{Univariate},
+    z::AbstractArray{<:Integer}, T::Integer,
+    N::Integer;
+)
+y = Array{Float64}(undef, size(z, 1), size(z, 2))
+for n in 1:N
+    for t in 1:T
+        y[t, n] = rand(rng, hmm.B[z[t]])
+    end
+end
+y
+end
+
+function rand(
+    rng::AbstractRNG,
+    hmm::AbstractHMM,
+    T::Integer,
+    N::Integer;
+    init = rand(rng, Categorical(hmm.a), N),
+    seq = false,
+)
+z = Matrix{Int}(undef, T, N)
+for n = 1:N
+    (T >= 1) && (N >= 1) && (z[1, n] = init[n])
+    for t = 2:T
+        z[t, n] = rand(rng, Categorical(hmm.A[z[t-1, n], :]))
+    end
+end
+y = rand(rng, hmm, z, T, N)
+seq ? (z, y) : y
+end
