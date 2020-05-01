7cb4410e2413b176d8c1522f444c5b8bb25fe0e3
Sosuke <uts.199.ubosou@gmail.com>
2020-05-01 12:15:37 +0900

    Add utility functions of baum welch

--- src/baum_welch.jl
+++ src/baum_welch.jl
@@ -1,12 +1,23 @@
+function data2matrix(
+        df::DataFrame, track_num::Int64, max_length::Int64,
+        track_length::AbstractArray, K::Int64, start_point::AbstractArray,
+    )
+    data = zeros(Float64, (max_length, K, track_num))
+    for i in 1:track_num
+        for n in 1:track_length[i]+1
+            data[n,:,i] .= df.dR[start_point[i]+n]
+        end
+    end
+    data
+end
+
 function create_prior(K::Int64, dt::Float64, df::DataFrames.DataFrame, error::Float64)
     a::Array{Float64,1} = rand(Float64, K)
     a /= sum(a)
-    
     A::Array{Float64,2} = rand(Float64, (K, K))
     @inbounds for i in 1:K
         A[i, :] /= sum(A[i, :])
     end
-    
     R = kmeans(filter(!isnan, abs2.(df.dR))', K, tol = 1e-6 ; maxiter = 10000)
     D = R.centers # get the cluster centers
     D /= 4dt
