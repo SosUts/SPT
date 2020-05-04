1708eb92bd7037fe9d4bbe984bc4f5e218df2731
Sosuke <uts.199.ubosou@gmail.com>
2020-05-04 18:31:25 +0900

    Add functions for angle calculation

--- src/angle.jl
+++ src/angle.jl
@@ -1,3 +1,19 @@
-function angle()
-    
+using LinearAlgebra
+
+# LinearAlgebra.cross couldn't be applied to 2d vector
+function twod_cross(
+    x::AbstractVector,
+    y::AbstractVector
+    )
+    x[1]*y[2]-x[2]*y[1]
+end
+
+function moving_angle(
+        x::AbstractVector,
+        y::AbstractVector
+    )
+    cos_theta = (x ⋅ y) / (norm(x)*norm(y))
+    theta = rad2deg(acos(cos_theta))
+    theta = ifelse(twod_cross(x,y)<0, -theta, theta)
+    return theta
 end
