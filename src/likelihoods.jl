b0b2e219127016c4bd8a59d83bc015790864171d
Sosuke <uts.199.ubosou@gmail.com>
2020-05-13 18:56:28 +0900

    Add functions for calculatinf likelihoods

---
+++ src/likelihoods.jl
@@ -0,0 +1,43 @@
+function likelihood!(
+    obserbations::AbstractArray,
+    d,
+    L::AbstractArray
+    )
+    @argcheck size(obserbations) == size(L)
+    K, N = size(L, 2), size(L, 3)
+    fill!(L, 0.0)
+
+    @inbounds for i in 1:N
+        track_length = count(!iszero, obserbations[:,1,i])
+        @inbounds for t in 1:track_length[i]
+            @inbounds for j in 1:K
+                L[t, j, i] = pdf.(
+                    d[j],
+                    ifelse(dR[t, j, i]<1e-4, 1e-4, dR[t, j, i])
+                    )
+            end
+        end
+    end
+end
+
+function likelihood!(
+    obserbations::AbstractArray,
+    d,
+    L::AbstractArray
+    )
+    @argcheck size(obserbations) == size(L)
+    K, N = size(L, 2), size(L, 3)
+    fill!(L, 0.0)
+
+    @inbounds for i in 1:N
+        track_length = count(!iszero, obserbations[:,1,i])
+        @inbounds for t in 1:track_length[i]
+            @inbounds for j in 1:K
+                L[t, j, i] = logpdf.(
+                    d[j],
+                    ifelse(dR[t, j, i]<1e-4, 1e-4, dR[t, j, i])
+                    )
+            end
+        end
+    end
+end
