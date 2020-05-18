3c3d2b8dd1bfcdde324b0a66ea355a974afd0dd4
Sosuke <uts.199.ubosou@gmail.com>
2020-05-18 14:51:47 +0900

    Choose functions to export

--- src/spt.jl
+++ src/spt.jl
@@ -13,33 +13,44 @@ using Statistics
 
 import Random: AbstractRNG, GLOBAL_RNG
 import StatsBase: sem
-import Distributions: quantile, minimum, maximum, pdf, cdf, @check_args
+import Distributions: quantile, minimum, maximum, pdf, cdf, rand, logpdf, @check_args, @distr_support
 
 # exportしたい関数一覧
 export
-    # msd.jl
-    # baum_welch.jl
-    # viterbi.jl
     # angle.jl
+    twod_cross,
+    moving_angle,
+    spiff,
+    # msd.jl
+    Diffusion,
+    # messages.jl
+    forward!,
+    backward!,
+    posterior!,
+    update_ξ!,
+    # likelihoods.jl
+    likelihood!,
+    loglikelihood!,
     # utilities.jl
+    create_prior,
+    preproccsing!,
+    data2matrix,
+    nomapround
 
 # 読み込みたいjlファイル
-include("hmm.jl")
+include("angle.jl")
+include("displacement.jl")
 include("mle.jl")
-include("mle_api.jl")
-include("mle_init.jl")
+include("msd.jl")
 include("messages.jl")
-include("messages_api.jl")
-include("viterbi.jl")
 include("viterbi_api.jl")
 include("likelihoods.jl")
-include("likelihoods_api.jl")
 include("utilities.jl")
 
 # To be removed in a future version
 # ---------------------------------
-export
-    n_parameters,
+# export
+    # n_parameters,
 
 # @deprecate n_parameters(hmm) nparams(hmm)
 
